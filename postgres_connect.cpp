7805374adb605a8b938e0bd630392a97b7575ae8
Moshe Shadmon <moshe@scaledb.com>
2017-09-11 11:58:01 -0700

    code regarding query

--- postgres_connect.cpp
+++ postgres_connect.cpp
@@ -154,58 +154,49 @@ void PostgresConnect::createJson( PGresult *res ){
 	int nfields, ntuples, i, j;
 	int retValue;
 	char *fieldName;
-
-
-Document doc;
-doc.SetObject();
-
-Document::AllocatorType& allocator = doc.GetAllocator();	//allocator for reallocating memory
-
-Value value_obj(kObjectType);
-
-value_obj.AddMember("string", "v1", allocator);
-value_obj.AddMember("int", 5, allocator);
-value_obj.AddMember("double", 12.04, allocator);
-doc.AddMember("obj", value_obj, allocator);
-doc.AddMember("int", 7, allocator);
-// Convert JSON document to string
-GenericStringBuffer< UTF8<> > buffer;
-Writer< GenericStringBuffer< UTF8<> > > writer(buffer);
-doc.Accept(writer);
-
-
-const char* str = buffer.GetString();
-
-printf("JSON: %s\n", str);
-
+	char *fieldValue;
+	
 	Document document;
 	document.SetObject();	// create a JSON document
-	
+
+	Document::AllocatorType& allocator = document.GetAllocator();	//allocator for reallocating memory
 
 	nfields = PQnfields(res);
 	ntuples = PQntuples(res);
-
-
-	 string sname = "test03";
 
 
 	for(i = 0; i < ntuples; i++){
 		for(j = 0; j < nfields; j++){
 			fieldType = PQftype(res, j);
 			fieldName = PQfname(res, j);
-			printf("\n[%d,%d] %s - %u", i, j, PQgetvalue(res, i, j), fieldType );
+			fieldValue = PQgetvalue(res, i, j);
+
+			printf("\n[%d,%d] %s - %u", i, j, fieldValue, fieldType );
 
 			switch (fieldType){	// Need to include pg_type.h - The list is available here - https://doxygen.postgresql.org/include_2catalog_2pg__type_8h_source.html 
 
 				case 20:		// dataType bigint (replace 20 with INT8OID)
+					{
+						string strName(fieldName);
+						int intValue = atoi(fieldValue);
+						Value attrName(strName.c_str(), allocator);
+						document.AddMember(attrName, intValue, allocator);
+					}
+					break;
+
+				case 1043:		// dataType character varying (replace 1043 with VARCHAROID)
+				case 2950:		// dataType uuid (replace 2950 with UUIDOID)
+					{
+						string strName(fieldName);
+						string strValue(fieldValue);
+						Value attrName(strName.c_str(), allocator);
+						Value attrValue(strValue.c_str(), allocator);
+						document.AddMember(attrName, attrValue, allocator);
+					}
 
 					break;
 				case 710:		// dataType float (replace 701 with FLOAT8OID)
-					value_obj.AddMember("string", "v1" , allocator);
-					break;
-				case 1043:		// dataType character varying (replace 1043 with VARCHAROID)
-					break;
-				case 2950:		// dataType uuid (replace 2950 with UUIDOID)
+//					value_obj.AddMember("string", "v1" , allocator);
 					break;
 				case 3802:		// dataType jsonb (replace 3802 with JSONBOID)
 					break;
@@ -217,6 +208,17 @@ printf("JSON: %s\n", str);
 			}
 		}
 	}
+
+// Convert JSON document to string
+GenericStringBuffer< UTF8<> > buffer;
+Writer< GenericStringBuffer< UTF8<> > > writer(buffer);
+document.Accept(writer);
+
+
+const char* str = buffer.GetString();
+
+printf("\nJSON: %s\n", str);
+printf("\nJSON: %s\n", str);
 
 }
 /*******************************************************************************************************//**
